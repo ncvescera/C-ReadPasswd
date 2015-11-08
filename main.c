0e577854bace8aa9afc6a390b10df35d6e63b557
Nicolò Vescera <slayercandy@icloud.com>
2015-11-08 11:25:46 +0100

    First Commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: root
+ *
+ * Created on 8 novembre 2015, 11.25
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
